3b1e7db89fb7f9cfe6f4397b893a2243646053bc
hacer <sakirogluhacer@gmail.com>
2025-12-05 15:53:40 +0300

    Add initial G-code parser module with basic structure and comment

--- software/gcode_parser.py
+++ software/gcode_parser.py
@@ -1,1 +1,6 @@
+# gcode_parser.py
+# This module will parse G-code commands such as:
+# - G0 (rapid move)
+# - G1 (linear interpolation)
+# The parser will convert commands into motor steps for the CNC Plotter.
 
